beddae96a8165b5877d021334cf11d27e3f74cf0
juanda <juan.pino@ucp.edu.co>
2022-11-01 17:33:41 -0500

    Suma y resta

--- src/pages/FirstPage.jsx
+++ src/pages/FirstPage.jsx
@@ -3,14 +3,17 @@ import { useContador } from "../hooks/useContador"
 //useState
 // REFACTOR
 export const FirstPage = () => {
-  const { valor, modificarEstado, reset} = useContador(100)
+  let a=100;
+  let b=10;
+  let c=1;
+  const { valor, modificarEstado, reset} = useContador(a)
   return (
     <>
       <h1>Contador mejorado</h1>
       <hr />
       <h1>{ valor }</h1>
-      <button onClick={() => modificarEstado(-1)}>-1</button>
-      <button onClick={() => modificarEstado(+1)}>+1</button>
+      <button onClick={() => modificarEstado(-b)}>RESTAR 10</button>
+      <button onClick={() => modificarEstado(+c)}>SUMAR 1</button>
       <button onClick={reset}>Reset</button>
     </>
   )
